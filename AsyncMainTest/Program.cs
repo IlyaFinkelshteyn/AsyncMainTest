abd271ae5accb4173c4fa59f0cb1ab95ca97a68b
ilyaf f <ilyaf@appveyor.com>
2017-12-29 16:15:32 -0800

    Initial commit

---
+++ AsyncMainTest/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AsyncMainTest
+{
+    class Program
+    {
+        static async Task Main()
+        {
+            string test = default;
+            Console.WriteLine(test == null);
+        }
+    }
+}
